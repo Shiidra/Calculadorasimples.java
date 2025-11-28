80a658ade7519abb924054213a324cbd00b8011c
Francisco alexandre <alexandre061102@gmail.com>
2025-11-28 16:50:14 -0300

    Add files via upload

---
+++ TestTDD.java
@@ -0,0 +1,19 @@
+package test;
+
+import core.Calculator;
+import core.History;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTDD {
+
+    @Test
+    void somaDeveFuncionar() {
+        History history = new History();
+        Calculator calc = new Calculator(history);
+
+        String resultado = calc.calculate(1, 5, 7);
+
+        assertEquals("5 + 7 = 12", resultado);
+    }
+}
