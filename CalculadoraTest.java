80b69a91db204d1ba134cb5a4a27b04857e53308
Francisco alexandre <alexandre061102@gmail.com>
2025-11-28 16:32:54 -0300

    Add files via upload

---
+++ CalculadoraTest.java
@@ -0,0 +1,17 @@
+import core.Calculator;
+import core.History;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    @Test
+    void somaDeveFuncionar() {
+        History history = new History();
+        Calculator calc = new Calculator(history);
+
+        String resultado = calc.calculate(1, 5, 7);
+
+        assertEquals("5 + 7 = 12", resultado);
+    }
+}
